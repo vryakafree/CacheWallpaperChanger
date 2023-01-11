9ad0c8bfec2ca6fc216d0b92313de7dc4abbd5f9
Trần Duy Anh <58522357+vryakafree@users.noreply.github.com>
2023-01-11 11:22:48 +0700

    Thêm mở lại các thư mục đang sử dụng ở minimize

--- DefaultWallpaper.bat
+++ DefaultWallpaper.bat
@@ -1,6 +1,17 @@
 @echo off
 setlocal EnableDelayedExpansion
 del C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Themes\CachedFiles\CachedImage_1920_1080_POS2.jpg
+
+powershell  @^(^(New-Object -com shell.application^).Windows^(^)^).Document.Folder.Self.Path >> prevfolderpaths.txt
+taskkill /im explorer.exe /f
+start explorer.exe
+FOR /F "tokens=*" %%f IN (prevfolderpaths.txt) DO (
+set "var=%%f"
+set "firstletters=!var:~0,2!"
+IF "!firstletters!" == "::" ( start /min shell:%%~f ) ELSE ( start /min "" "%%~f" )
+)
+del "prevfolderpaths.txt"
+
 rd /s /q C:\$Recycle.Bin
-tskill explorer 
+
 exit
