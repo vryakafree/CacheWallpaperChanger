28749ba2283a502ccfd7f4189bdd0fa0619ad577
Trần Duy Anh <58522357+vryakafree@users.noreply.github.com>
2023-01-11 11:19:42 +0700

    fix username

--- DefaultWallpaper.bat
+++ DefaultWallpaper.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
-del C:\Users\cevdatran1\AppData\Roaming\Microsoft\Windows\Themes\CachedFiles\CachedImage_1920_1080_POS2.jpg
+del C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Themes\CachedFiles\CachedImage_1920_1080_POS2.jpg
 rd /s /q C:\$Recycle.Bin
 tskill explorer 
 exit
